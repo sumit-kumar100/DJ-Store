184bb477e6f572db66bafde87af22d83726b2fd8
Sumit Singh <sumit.kumar@dahatconsultancy.in>
2023-06-19 14:10:26 +0530

    [ADD] vercel-settings added

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python3.8 -m pip install -r requirements.txt
-python3.8 manage.py collectstatic --noinput --clear
+python3.7 -m pip install -r requirements.txt
+python3.7 manage.py collectstatic --noinput --clear
 echo "BUILD END"
